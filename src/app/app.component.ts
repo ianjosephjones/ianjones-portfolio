5e70bb5c172c4b0702ac35a332bbc440cd12048f
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-09-23 14:32:06 -0400

    Data driven nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,26 +61,6 @@ export class AppComponent implements OnInit {
   }
 
   /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach((navbarlink: { hash: any; classList: { add: (arg0: string) => void; remove: (arg0: string) => void; }; }) => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el:any) => {
--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
 })
 export class SideNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  navItems: Array<NavItem>;
+  constructor() {
+    this.navItems = [
+      new NavItem('Home', 'bi-house-fill', '#hero', true),
+      new NavItem('About', 'bi-person', '#about'),
+      new NavItem('Resume', 'bi-file-text', '#resume'),
+      new NavItem('Portfolio', 'bi-layout-text-window-reverse', '#portfolio'),
+    ];
   }
+  ngOnInit(): void {}
 
+  navItemClick(item: NavItem) {
+    this.navItems.forEach((e) => {
+      if (e.id !== item.id) e.active = false;
+      else e.active = true;
+    });
+  }
+}
+
+class NavItem {
+  description: string;
+  aClassName = 'nav-link scrollto';
+  iconName: string;
+  id: string;
+  active = false;
+  constructor(
+    description: string,
+    iconName: string,
+    id: string,
+    active: boolean = false
+  ) {
+    this.description = description;
+    this.iconName = iconName;
+    this.id = id;
+    this.active = active;
+  }
 }
